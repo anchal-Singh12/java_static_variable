fbfdb32bf9add251df911d0744d6e08734f88b12
anchal-Singh12 <anchal923675@gmail.com>
2025-10-27 20:42:22 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,12 @@
+ class Square {
+    //satic method
+    static int square(int x){
+        return x*x;
+    }
+    
+}
+public class test{
+    public static void main(String[] args){
+        System.out.println(Square.square(5));
+    }
+}
